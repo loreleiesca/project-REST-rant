607eb5bb188a7281b0704ec9e8e39504415471d4
loreleiesca <110075659+loreleiesca@users.noreply.github.com>
2023-10-18 18:15:22 -0700

    Currently finishing up part 5

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,7 +6,30 @@ function new_form () {
         <Def>
           <main>
             <h1>Add a New Place</h1>
-          </main>
+            <form method="POST" action="/places">
+            <div className="form-group">
+                <label htmlFor="name">Place Name</label>
+                <input id="name" name="name" required/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="pic">Place Picture</label>
+                <input type="url" id="pic" name="pic" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="city">City</label>
+                <input id="city" name="city" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="state">State</label>
+                <input id="state" name="state" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="cuisines">Cuisines</label>
+                <input id="cuisines" name="cuisines" required/>
+            </div>
+                 <input type="submit" value="Add Place" />
+            </form>
+            </main>
         </Def>
     )
 }
